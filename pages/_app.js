ca50084e46711cd769983ff1fb60e843e2c04d17
Eftekar Raghib <70286969+juniorEdris@users.noreply.github.com>
2022-09-23 20:30:44 +0600

    ndesign addedv

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import { Provider } from "react-redux";
+import PrimaryHeader from "../components/PrimaryHeader";
 import store from "../store";
 import "../styles/globals.css";
-// import "../node_modules/bootstrap/dist/js/bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <div className="header">
+        <PrimaryHeader />
+      </div>
       <Component {...pageProps} />
     </Provider>
   );
