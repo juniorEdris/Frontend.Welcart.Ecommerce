a6afe354b89717d3deb0e16af796ef40fc8de75f
Eftekar Raghib <70286969+juniorEdris@users.noreply.github.com>
2022-09-23 16:47:43 +0600

    updated design

--- components/CategorySection.js
+++ components/CategorySection.js
@@ -1,16 +1,114 @@
+import Image from "next/image";
+
 const ProductCategory = () => {
   return (
-    <div className="primary-dark-font">
+    <div className="primary-dark-font px-2 md:p-0">
       <h1 className="text-3xl font-bold mb-3">Popular Product Categories</h1>
-      <div className="grid grid-cols-2 md:grid-cols-4">
-        <div className="">01</div>
-        <div className="">02</div>
-        <div className="">03</div>
-        <div className="">04</div>
-        <div className="">05</div>
-        <div className="">06</div>
-        <div className="">07</div>
-        <div className="">08</div>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        <div className="flex items-center justify-center light-bg py-2 rounded cursor-pointer">
+          <div className="mx-3">
+            <Image
+              src="/img/productcategory/01.png"
+              alt=""
+              height="40"
+              width="40"
+            />
+          </div>
+          <span className="text-base font-semibold capitalize">
+            electronics appliances
+          </span>
+        </div>
+        <div className="flex items-center justify-center light-bg py-2 rounded cursor-pointer">
+          <div className="mx-3">
+            <Image
+              src="/img/productcategory/02.png"
+              alt=""
+              height="40"
+              width="40"
+            />
+          </div>
+          <span className="text-base font-semibold capitalize">
+            electronics appliances
+          </span>
+        </div>
+        <div className="flex items-center justify-center light-bg py-2 rounded cursor-pointer">
+          <div className="mx-3">
+            <Image
+              src="/img/productcategory/03.png"
+              alt=""
+              height="40"
+              width="40"
+            />
+          </div>
+          <span className="text-base font-semibold capitalize">
+            electronics appliances
+          </span>
+        </div>
+        <div className="flex items-center justify-center light-bg py-2 rounded cursor-pointer">
+          <div className="mx-3">
+            <Image
+              src="/img/productcategory/04.png"
+              alt=""
+              height="40"
+              width="40"
+            />
+          </div>
+          <span className="text-base font-semibold capitalize">
+            electronics appliances
+          </span>
+        </div>
+        <div className="flex items-center justify-center light-bg py-2 rounded cursor-pointer">
+          <div className="mx-3">
+            <Image
+              src="/img/productcategory/05.png"
+              alt=""
+              height="40"
+              width="40"
+            />
+          </div>
+          <span className="text-base font-semibold capitalize">
+            electronics appliances
+          </span>
+        </div>
+        <div className="flex items-center justify-center light-bg py-2 rounded cursor-pointer">
+          <div className="mx-3">
+            <Image
+              src="/img/productcategory/06.png"
+              alt=""
+              height="40"
+              width="40"
+            />
+          </div>
+          <span className="text-base font-semibold capitalize">
+            electronics appliances
+          </span>
+        </div>
+        <div className="flex items-center justify-center light-bg py-2 rounded cursor-pointer">
+          <div className="mx-3">
+            <Image
+              src="/img/productcategory/07.png"
+              alt=""
+              height="40"
+              width="40"
+            />
+          </div>
+          <span className="text-base font-semibold capitalize">
+            electronics appliances
+          </span>
+        </div>
+        <div className="flex items-center justify-center light-bg py-2 rounded cursor-pointer">
+          <div className="mx-3">
+            <Image
+              src="/img/productcategory/08.png"
+              alt=""
+              height="40"
+              width="40"
+            />
+          </div>
+          <span className="text-base font-semibold capitalize">
+            electronics appliances
+          </span>
+        </div>
       </div>
     </div>
   );
